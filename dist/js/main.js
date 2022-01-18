ff2f930b6c40a9917ad0ce2287baac4045677d9d
bumblybee <hesstjune@gmail.com>
2022-01-18 15:06:14 -0600

    Semantic html tags. Cleanup

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,80 +1,78 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBg = document.querySelector(".menu-bg");
-const menuItems = document.querySelectorAll(".menu-item");
-const phone = document.querySelector(".phone");
+const hamburger = document.querySelector(".hamburger")
+const menu = document.querySelector(".menu")
+const menuNav = document.querySelector(".menu-nav")
+const menuBg = document.querySelector(".menu-bg")
+const menuItems = document.querySelectorAll(".menu-item")
+const phone = document.querySelector(".phone")
 
-let showMenu = false;
+let showMenu = false
 
-window.addEventListener("load", setImageWidth);
-hamburger.addEventListener("click", toggleMenu);
-phone.addEventListener("click", copyPhoneNumber);
+window.addEventListener("load", setImageWidth)
+hamburger.addEventListener("click", toggleMenu)
+phone.addEventListener("click", copyPhoneNumber)
 
 function toggleMenu() {
   if (!showMenu) {
-    hamburger.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBg.classList.add("show");
+    hamburger.classList.add("close")
+    menu.classList.add("show")
+    menuNav.classList.add("show")
+    menuBg.classList.add("show")
 
-    menuItems.forEach((item) => item.classList.add("show"));
+    menuItems.forEach((item) => item.classList.add("show"))
 
-    showMenu = true;
+    showMenu = true
   } else {
-    hamburger.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuBg.classList.remove("show");
+    hamburger.classList.remove("close")
+    menu.classList.remove("show")
+    menuNav.classList.remove("show")
+    menuBg.classList.remove("show")
 
-    menuItems.forEach((item) => item.classList.remove("show"));
+    menuItems.forEach((item) => item.classList.remove("show"))
 
-    showMenu = false;
+    showMenu = false
   }
 }
 
 function copyPhoneNumber() {
-  const phoneNum = phone.textContent.split(":")[1].trim();
+  const phoneNum = phone.textContent.split(":")[1].trim()
   navigator.permissions.query({ name: "clipboard-write" }).then((res) => {
     if (res.state == "granted" || res.state == "prompt") {
-      updateClipboard(phoneNum);
+      updateClipboard(phoneNum)
     }
-  });
+  })
 }
 
 function updateClipboard(clip) {
   navigator.clipboard.writeText(clip).then(() => {
-    showCopiedMessage("success");
+    showCopiedMessage("success")
   }),
     () => {
-      showCopiedMessage("fail");
-    };
+      showCopiedMessage("fail")
+    }
 }
 
 function showCopiedMessage(state) {
-  let msg;
+  let msg
 
   if (state === "success") {
-    msg = "Phone number copied";
+    msg = "Phone number copied"
   } else {
-    msg = "Unable to copy phone number";
+    msg = "Unable to copy phone number"
   }
 
-  phone.textContent = msg;
+  phone.textContent = msg
 
   setTimeout(() => {
-    const span = document.createElement("span");
-    span.textContent = "Phone: ";
-    span.className = "text-tertiary";
-    phone.textContent = "(319) 239-3607";
-    phone.prepend(span);
-  }, 2000);
+    const span = document.createElement("span")
+    span.textContent = "Phone: "
+    span.className = "text-tertiary"
+    phone.textContent = "(319) 239-3607"
+    phone.prepend(span)
+  }, 2000)
 }
 
 function setImageWidth() {
-  const bg = document.querySelector("body#bg");
+  const bg = document.querySelector("body#bg")
   bg.style.backgroundImage =
-    "-webkit-image-set(url('https://ik.imagekit.io/dzs7huqkhng/codebkgrd_ODy65dcT9.webp?tr=w-2000') 1x, url('https://ik.imagekit.io/dzs7huqkhng/codebkgrd_ODy65dcT9.webp?tr=w-1024') 2x, url('https://ik.imagekit.io/dzs7huqkhng/codebkgrd_ODy65dcT9.webp?tr=w-600') 3x)";
-
-  console.log("yup");
+    "-webkit-image-set(url('https://ik.imagekit.io/dzs7huqkhng/codebkgrd_ODy65dcT9.webp?tr=w-2000') 1x, url('https://ik.imagekit.io/dzs7huqkhng/codebkgrd_ODy65dcT9.webp?tr=w-1024') 2x, url('https://ik.imagekit.io/dzs7huqkhng/codebkgrd_ODy65dcT9.webp?tr=w-600') 3x)"
 }
